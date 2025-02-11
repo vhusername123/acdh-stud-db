8acfff2f074fcded081d4a0d42749b8af3642732
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-11 14:47:35 +0100

    some

--- bin/2024-deduplicate/test.ts
+++ bin/2024-deduplicate/test.ts
@@ -8,7 +8,7 @@ const exampledates = [
 ];
 
 describe("DateRange", () => {
-  test("should correctly initialize start and end dates", () => {
+  test("initialize start and end dates", () => {
     const date1 = new Date(exampledates[0]); // 1900-01-01
     const date2 = new Date(exampledates[1]); // 1900-12-31
 
@@ -18,7 +18,7 @@ describe("DateRange", () => {
     expect(range.startdate).toBe(date1);
     expect(range.enddate).toBe(date2);
   });
-  test("should correctly initialize start and end dates", () => {
+  test("initialize start and end dates", () => {
     const date1 = new Date(exampledates[0]); // 1900-01-01
     const date2 = new Date(exampledates[2]); // 1900-06-01
       
@@ -29,7 +29,7 @@ describe("DateRange", () => {
     expect(range.startdate).toBe(date1);
     expect(range.enddate).toBe(date2);
   });
-  test("should correctly initialize start and end dates", () => {
+  test("initialize start and end dates", () => {
     const date1 = new Date(exampledates[3]); // 1901-05-30
     const date2 = new Date(exampledates[3]); // 1901-05-30
       
@@ -40,7 +40,7 @@ describe("DateRange", () => {
     expect(range.startdate).toBe(date1);
     expect(range.enddate).toBe(date2);
   });
-  test("should correctly initialize start and end dates", () => {
+  test("initialize start and end dates", () => {
     const date1 = new Date(exampledates[1]); // 1900-12-31
     const date2 = new Date(exampledates[2]); // 1900-06-01
     date1.setDate(date1.getDate() + 1);
@@ -49,7 +49,7 @@ describe("DateRange", () => {
     expect(range.startdate).toBe(date2);
     expect(range.enddate).toBe(date1);
   });
-  test("should correctly initialize start and end dates", () => {
+  test("initialize start and end dates", () => {
     const date1 = new Date(exampledates[2]); // 1900-06-01
     const date2 = new Date(exampledates[3]); // 1901-05-30
     date2.setDate(date2.getDate() + 1);
@@ -58,7 +58,7 @@ describe("DateRange", () => {
     expect(range.startdate).toBe(date1);
     expect(range.enddate).toBe(date2);
   });
-  test("should correctly fail to initialise start and end dates", () => {
+  test("fail to initialise start and end dates", () => {
     const date1 = new Date(exampledates[3]); // 1901-05-30
     const date2 = new Date(exampledates[4]); // 1900-06-00
     const range = new DateRange(date1, date2);
@@ -68,7 +68,7 @@ describe("DateRange", () => {
     expect(NaN).toBe(date2.getDate());
   });
 
-  test("should correctly calculate length in days", () => {
+  test("calculate length in days", () => {
     const date1 = new Date(exampledates[0]); // 1900-01-01
     const date2 = new Date(exampledates[1]); // 1900-12-31
     const range = new DateRange(date1, date2);
@@ -76,7 +76,7 @@ describe("DateRange", () => {
     expect(range.lengthindays).toBe(365);
   });
 
-  test("should correctly identify overlapping ranges", () => {
+  test("identify overlapping ranges", () => {
     const range1 = new DateRange(
       new Date(exampledates[0]), // 1900-01-01
       new Date(exampledates[1]) // 1900-12-31
@@ -89,7 +89,7 @@ describe("DateRange", () => {
     expect(range1.hasOverlap(range2)).toBe(true);
   });
 
-  test("should correctly identify non-overlapping ranges", () => {
+  test("identify non-overlapping ranges", () => {
     const range1 = new DateRange(
       new Date(exampledates[0]), // 1900-01-01
       new Date(exampledates[2]) // 1900-06-01
@@ -102,7 +102,7 @@ describe("DateRange", () => {
     expect(range1.hasOverlap(range2)).toBe(false);
   });
 
-  test("should correctly calculate overlap in days", () => {
+  test("calculate overlap in days", () => {
     const range1 = new DateRange(
       new Date(exampledates[0]), // 1900-01-01
       new Date(exampledates[1]) // 1900-12-31
@@ -116,7 +116,7 @@ describe("DateRange", () => {
     expect(range2.overlap(range1)).toBe(214);
   });
 
-  test("should correctly calculate overlap percentage", () => {
+  test("calculate overlap percentage", () => {
     const range1 = new DateRange(
       new Date(exampledates[0]), // 1900-01-01
       new Date(exampledates[1]) // 1900-12-31
@@ -131,7 +131,7 @@ describe("DateRange", () => {
     expect(range2.overlapPercentage(overlap)).toBeCloseTo(0.5879, 3);
   });
 
-  test("should correctly calculate ranged disparity", () => {
+  test("calculate ranged disparity", () => {
     const range1 = new DateRange(
       new Date(exampledates[0]), // 1900-01-01
       new Date(exampledates[1]) // 1900-12-31
@@ -144,7 +144,7 @@ describe("DateRange", () => {
     expect(range1.rangeLengthDisparity(range2)).toBe(0);
   });
 
-  test("should correctly calculate ranged disparity", () => {
+  test("calculate ranged disparity", () => {
     const range1 = new DateRange(
         new Date(exampledates[0]), // 1900-01-01
         new Date(exampledates[1]) // 1900-12-31

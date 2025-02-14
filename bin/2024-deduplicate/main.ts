e0d8e16d4b7f05d3126a606d06be49ce17ddd964
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-14 16:26:33 +0100

    small worker logic changes

---
+++ bin/2024-deduplicate/main.ts
@@ -0,0 +1,64 @@
+import { getbatches } from "./createbatches";
+import fs from "node:fs";
+import { promiseWorker } from "./mainWorker";
+const path = "ids.json";
+const workerpath = "./worker.js";
+const credentials = {
+  host: "localhost",
+  port: 13006,
+  database: "rksd",
+  charset: "utf8",
+  user: "rksd",
+  password: "nJkyj2pOsfUi",
+};
+if (fs.existsSync(path)) {
+  fs.rmSync(path, { force: true });
+}
+//function to read the json file to create as many workers as needed
+function createworker() {
+  let idfile = fs.readFileSync(path, "ascii").split("\n");
+  const workers = [];
+  for (let i = 1; i < idfile.length - 1; i++) {
+    const idsstring = idfile[i].substring(1, idfile[i].length - 1);
+    const ids: number[] = idsstring.split(",").map((id) => {
+      return parseInt(id);
+    });
+    workers.push(promiseWorker(ids, credentials, workerpath));
+  }
+  
+  Promise.all(workers).then((test) => {
+    test.forEach((e) => console.log(e))
+  });
+}
+
+//get (8, 125 ids) batches and then create workers
+getbatches(credentials, 8, 10).then(() => createworker());
+
+/* original
+createConnection(credentials).then((connection) =>
+  getHighestAvailableIds(connection)
+    .then((limits) => findBatchIds(connection, limits, 10))
+    .then((ids) => {
+      console.log(ids[0], "/", ids[1], "..", ids[ids.length - 1]);
+      return ids;
+    })
+    .then((ids) => loadBatchOfPropertyRecords(connection, ids))
+    .then(reducePropertyRecordsToPeople)
+    .then(computeStats)
+    .then((comparisons) => writeComparisonBatch(connection, comparisons))
+    .then(() => connection.end()).catch((message) => console.error(message)),
+);
+*/
+/* future implementation
+
+async function createworker() {
+  const jsonData = await importJsonFile("./ids.json");
+  jsonData.allids.forEach((e) => {
+    if (e.done === true) return;
+    const ids: number[] = e.ids.map((id) => {
+      return parseInt(id);
+    });
+    run(ids, credentials, workerpath);
+  });
+}
+  */

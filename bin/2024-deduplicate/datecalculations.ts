a1dc084961f86175d59799f9fd96ee732a04b6d1
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-17 13:10:42 +0100

    compile personbirthdates into a DateRangeArray

--- bin/2024-deduplicate/datecalculations.ts
+++ bin/2024-deduplicate/datecalculations.ts
@@ -1,17 +1,32 @@
 import { textSpanOverlap } from "typescript";
 import { DateRange } from "./DateRange";
 //import { DateRangeComparisonStats } from "./types";
+
+const personBirthRanges = ([bornafter,bornbefore]:Date[][]) => {
+    const ranges:DateRange[] = []
+    for(let i = 0; i<bornafter.length;i++) {
+        let after = bornafter[i],before = bornbefore[i],originaldate:Date|boolean = false;
+        if(after === before) {
+            originaldate = new Date(before)
+            before = new Date(before.getTime() - 180 *DateRange.millisecondsPerDay)
+            after = new Date(after.getTime() + 179 *DateRange.millisecondsPerDay)
+        }
+
+        ranges.push(DateRange.create(after,before,originaldate))
+    }
+    return ranges
+}
 const exampledates = [
-    "1900-01-01",
-    "1900-12-31",
-    "1900-06-01",
-    "1901-05-30",
-    "1900-06-20",
+    new Date("1900-01-01"),
+    new Date("1900-12-31"),
+    new Date("1900-06-01"),
+    new Date("1901-05-30"),
+    new Date("1900-06-20"),
   ];
 
 function compare(person1:DateRange[], person2:DateRange[]){
-    const comparison: DateRangeComparisonStats[][] = 
-        person1.map((e,i,a) => {
+    const comparison: DateRangeComparisonStats[] = 
+        person1.flatMap((e,i,a) => {
             return person2.map((e2) => {
                 const comparison1:DateRangeComparisonStats = {
                     overlap: 0,
@@ -28,12 +43,18 @@ function compare(person1:DateRange[], person2:DateRange[]){
         })
     return comparison
 }
-const testperson = exampledates.map((e,i,a) => DateRange.create(new Date(e),new Date(a[a.length-1-i])))
-const testperson2 = exampledates.map ((e,i,a) => DateRange.create(new Date(a[a.length-1-i]),new Date(a[a.length-1])))
-console.log(compare(testperson,testperson2))
 
+const testperson:DateRange[] = personBirthRanges([exampledates,exampledates])
+const testperson2:DateRange[] = personBirthRanges([exampledates,exampledates])
+console.log(testperson)
+console.log(testperson2)
 type DateRangeComparisonStats = {
     overlap: number;
     overlapPercentage: number;
     lengthdisparity: number;
   }
+type DateRangeComparisonBools = {
+    hasOverlap: boolean,
+    inside: boolean,
+
+}

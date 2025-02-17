51d96c19936ec8d3c8362c036734c049248ba3a6
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-17 15:19:03 +0100

    compare function returns Stats Type

--- bin/2024-deduplicate/datecalculations.ts
+++ bin/2024-deduplicate/datecalculations.ts
@@ -1,60 +1,71 @@
-import { textSpanOverlap } from "typescript";
+import {Stats} from './types'
 import { DateRange } from "./DateRange";
 //import { DateRangeComparisonStats } from "./types";
 
-const personBirthRanges = ([bornafter,bornbefore]:Date[][]) => {
+const personBirthRanges = (dates:Date[][]) => {
     const ranges:DateRange[] = []
-    for(let i = 0; i<bornafter.length;i++) {
-        let after = bornafter[i],before = bornbefore[i],originaldate:Date|boolean = false;
-        if(after === before) {
+    dates.forEach((e) => {
+        let after = new Date(e[0]),before = new Date(e[1]),originaldate:Date|boolean = false;
+        if(after.getTime() == before.getTime()) {
             originaldate = new Date(before)
             before = new Date(before.getTime() - 180 *DateRange.millisecondsPerDay)
             after = new Date(after.getTime() + 179 *DateRange.millisecondsPerDay)
         }
 
         ranges.push(DateRange.create(after,before,originaldate))
-    }
+    })
     return ranges
 }
+
+function compare(person1:DateRange[], person2:DateRange[]):Stats{
+    let median:number = 0,minimum:number = Infinity,maximum:number = 0,count:number=0;
+    let array:number[] = person1.flatMap((n) => {
+        const innermean = person2.map((e) => {
+            let f;
+            try {
+                f = e.overlap(n) / e.uniteDateRange(n).getLength()
+            }catch{
+                f = 0;
+            }
+            count++;
+            return f;
+          })
+        return innermean
+    })
+    let mean:number = array.reduce((sum,e) => sum+e,0) / count;
+    array.sort((a,b) => a-b)
+    minimum = Math.min(...array)
+    maximum = Math.max(...array)
+    median = array.length % 2 
+        ? (array[Math.floor(array.length/2)] + array[Math.floor(array.length/2)+1]) / 2
+        : array[array.length/2]
+    return [
+        mean,
+        median,
+        minimum,
+        maximum,
+        count
+    ]
+}
+
 const exampledates = [
-    new Date("1900-01-01"),
-    new Date("1900-12-31"),
-    new Date("1900-06-01"),
-    new Date("1901-05-30"),
-    new Date("1900-06-20"),
+    [new Date("1902-01-01"),new Date("1900-01-01")],
+    [new Date("1904-01-01"),new Date("1900-01-01")],
+    [new Date("1901-03-01"),new Date("1900-02-01")],
+    [new Date("1905-01-01"),new Date("1900-01-01")]
+  ];
+const exampledates2 = [
+    [new Date("1900-01-01"),new Date("1900-01-01")],
+    [new Date("1900-01-01"),new Date("1900-01-01")],
   ];
 
-function compare(person1:DateRange[], person2:DateRange[]){
-    const comparison: DateRangeComparisonStats[] = 
-        person1.flatMap((e,i,a) => {
-            return person2.map((e2) => {
-                const comparison1:DateRangeComparisonStats = {
-                    overlap: 0,
-                    overlapPercentage: 0,
-                    lengthdisparity: 0
-                };
-                try {
-                    comparison1.overlap = e2.overlap(e)
-                    comparison1.overlapPercentage = e2.overlap(e) / e2.getLength()
-                }catch{}
-                comparison1.lengthdisparity = e2.rangeLengthDisparity(e)
-                return comparison1
-            })
-        })
-    return comparison
-}
+const testperson:DateRange[] = personBirthRanges(exampledates)
+const testperson2:DateRange[] = personBirthRanges(exampledates2)
 
-const testperson:DateRange[] = personBirthRanges([exampledates,exampledates])
-const testperson2:DateRange[] = personBirthRanges([exampledates,exampledates])
+
 console.log(testperson)
 console.log(testperson2)
-type DateRangeComparisonStats = {
-    overlap: number;
-    overlapPercentage: number;
-    lengthdisparity: number;
-  }
-type DateRangeComparisonBools = {
-    hasOverlap: boolean,
-    inside: boolean,
-
-}
+console.log(compare(testperson,testperson2))
+console.log(compare(testperson2,testperson))
+/*
+*/

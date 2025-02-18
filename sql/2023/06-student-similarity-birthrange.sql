073f5fe2c1aef8f4f4dc539c5375e65d8ca4c5f9
vhusername123 <191569754+vhusername123@users.noreply.github.com>
2025-02-18 11:44:36 +0100

    prettier and update SQL table structure

--- sql/2023/06-student-similarity-birthrange.sql
+++ sql/2023/06-student-similarity-birthrange.sql
@@ -1,9 +1,14 @@
 DROP TABLE IF EXISTS student_similarity_birthrange;
-CREATE TABLE student_similarity_birthrange(  
-    id_low INT,
-    id_high INT,
-    overlap INT,
-    percentagelow FLOAT,
-    percentagehigh FLOAT,
-    rangedisparity FLOAT
-);
+CREATE TABLE student_similarity_birthrange
+(
+    id_low   bigint unsigned not null,
+    id_high  bigint unsigned not null,
+    mean     double unsigned not null default 0,
+    median   double unsigned not null default 0,
+    min      double unsigned not null default 0,
+    max      double unsigned not null default 0,
+    count    int unsigned    not null default 0,
+    key (id_low),
+    key (id_high),
+    unique key (id_low, id_high)
+)
